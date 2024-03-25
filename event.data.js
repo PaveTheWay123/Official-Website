f48e1fa2c5bd60da2a1bca1b6661324c9546d2f9
PaveTheWay123 <145612808+PaveTheWay123@users.noreply.github.com>
2024-03-25 12:50:30 +0200

    Update event.data.js

--- event.data.js
+++ event.data.js
@@ -10,8 +10,49 @@
 const EVENT_DATA = [
     {
         "name": "First Event",
-        "dateString": "27th of July 2024",
-        "description": "Event description",
+        "dateString": "8th of June 2023",
+        "description": "Rawane's Gig at Twenty-Two Gemmeyze",
         "src": "images/Event-1.jpeg"
-    }
+    },
+
+    {
+        "name": "Second Event",
+        "dateString": "7th of July 2023",
+        "description": "JP Sikias's performance at STOA Mar Mkhael",
+        "src": "images/Event-2.jpeg"
+    },
+
+    {
+        "name": "Third Event",
+        "dateString": "27th - 30th of July 2023",
+        "description": "Bake Sale at Diaspora Village Batroun",
+        "src": "images/Event-3.jpeg"
+    },
+
+    {
+        "name": "Fourth Event",
+        "dateString": "12th - 14th of August 2023",
+        "description": "Bake Sale at Jezzine",
+        "src": "images/Event-4.jpeg"
+    },
+
+    {
+        "name": "Fifith Event",
+        "dateString": "22nd of October 2023",
+        "description": "Rawane's Gig at Twenty-Two Gemmeyze",
+        "src": "images/Event-5.jpeg"
+    },
+
+    {
+        "name": "Sixth Event",
+        "dateString": "11th of December 2023",
+        "description": "Theatre Play at ALBA",
+        "src": "images/Event-6.jpeg"
+    },
+
+    {
+        "name": "Up Coming Events",
+        "description": "Stay Tuned for Up Coming Events",
+        "src": "images/pave-the-way-logo-2.jpeg"
+    },
     ];
